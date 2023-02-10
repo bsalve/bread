439f40ca6cb43a17e626097a1e8092b98ec2243e
bsalve <brandonsalve@gmail.com>
2023-02-09 17:51:01 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, redirect, render_template, request, make_response, jsonify
+from riotwatcher import ApiError
+import time
+
+from helpers import get_ranked_stats, get_match_stats, update_static_data, watcher, region
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+matches = []
+quantity = 5
+posts = 20
+
+@app.route("/")
+def index():
+
+    update_static_data(region)
+
+    return render_template("index.html")
+
+@app.route("/profile")
+def profile():
+
+    global matches
+
+    matches.clear()
+
+    query = request.args.get("search")
+
+    if not query:
+
+        return redirect(request.referrer)
+
+    try: 
+
+        summoner = watcher.summoner.by_name(region, query)
+
+    except ApiError as err:
+
+        error_code = err.response.status_code
+
+        if error_code == 403:
+
+            return render_template("error.html", error_message = "Refused Access (Invalid API key)", error_code = error_code)
+
+        elif error_code == 429: 
+
+            return render_template("error.html", error_message = "Too many requests. Try again later.", error_code = error_code)
+
+        elif error_code == 404: 
+
+            return render_template("error.html", error_message = "Summoner not found.", error_code = error_code)
+
+        else: 
+
+            raise
+
+    ranked_stats = watcher.league.by_summoner(region, summoner["id"])
+
+    soloq = "Unranked"
+    flexq = "Unranked"
+
+    for queue_type in range(len(ranked_stats)):
+
+        if ranked_stats[queue_type]["queueType"] == "RANKED_SOLO_5x5":
+
+            soloq = get_ranked_stats(queue_type, ranked_stats)
+        
+        elif ranked_stats[queue_type]["queueType"] == "RANKED_FLEX_SR":
+
+            flexq = get_ranked_stats(queue_type, ranked_stats)
+
+    match_list = watcher.match.matchlist_by_puuid(region, summoner["puuid"])
+
+    for i in range(posts):
+
+        matches.append(get_match_stats(i, match_list, region))
+
+    return render_template("profile.html", summoner = summoner, soloq = soloq, flexq = flexq, matches = matches)
+
+@app.route("/load")
+def load():
+
+    time.sleep(0.2)
+
+    if request.args:
+
+        counter = int(request.args.get("c"))
+
+        if counter == 0:
+            
+            print(f"Returning posts 0 to {quantity}")
+
+            res = make_response(jsonify(matches[0: quantity]), 200)
+
+        elif counter == posts:
+
+            print("No more posts")
+
+            res = make_response(jsonify({}), 200)
+
+        else:
+
+            print(f"Returning posts {counter} to {counter + quantity}")
+
+            res = make_response(jsonify(matches[counter: counter + quantity]), 200)
+
+    return res
---
+++ helpers.py
@@ -0,0 +1,90 @@
+from riotwatcher import LolWatcher
+
+watcher = LolWatcher("RGAPI-fbbbb97f-ef2f-4871-9ddf-f797b4b68eb7")
+region = "na1"
+
+champ_dict = {}
+ss_dict = {}
+
+# Updates champion dictionary and summoner spell dictionaries with live data
+def update_static_data(region):
+
+    latest = watcher.data_dragon.versions_for_region(region)["n"]["champion"]
+
+    static_champ_list = watcher.data_dragon.champions(latest, False, "en_US")
+
+    for key in static_champ_list["data"]:
+
+        row = static_champ_list["data"][key]
+
+        champ_dict[row["key"]] = row["id"]
+
+    static_ss_list = watcher.data_dragon.summoner_spells(latest, False)
+
+    for key in static_ss_list["data"]:
+
+        row = static_ss_list["data"][key]
+
+        ss_dict[row["key"]] = row["id"]
+
+# Returns a dictionary of ranked stats, takes in queue_type and summoner parameters
+def get_ranked_stats(queue_type, summoner):
+
+    ranked_stats = {}
+
+    ranked_stats["tier"] = str(summoner[queue_type]["tier"])
+    ranked_stats["rank"] = str(summoner[queue_type]["rank"])
+    ranked_stats["lp"] = str(summoner[queue_type]["leaguePoints"])
+
+    wins = str(summoner[queue_type]["wins"])
+    losses = str(summoner[queue_type]["losses"])
+    
+    ranked_stats["wins"] = wins
+    ranked_stats["losses"] = losses
+    ranked_stats["winrate"] = str(round((float(wins)/float(int(wins)+int(losses))*100))) + "%"
+
+    return ranked_stats
+
+# Returns a dictionary of "metadata" and "info", with information on participants, runes, champs, etc.
+def get_match_stats(match_number, match_list, region):
+
+    match_detail = watcher.match.by_id(region, match_list[match_number])
+
+    match = {}
+
+    match["metadata"] = []
+    match["info"] = []
+
+    for row in match_detail["info"]["participants"]:
+
+        participant = {}
+
+        participant["name"] = row["summonerName"]
+        participant["champion"] = row["championId"]
+        participant["ss1"] = row["summoner1Id"]
+        participant["ss2"] = row["summoner2Id"]
+        participant["role"] = row["teamPosition"]
+        participant["kills"] = row["kills"]
+        participant["deaths"] = row["deaths"]
+        participant["assists"] = row["assists"]
+        participant["win"] = row["win"]
+        participant["cs"] = row["totalMinionsKilled"]
+        participant["visionScore"] = row["visionScore"]
+        participant["damage"] = row["totalDamageDealtToChampions"]
+        participant["goldEarned"] = row["goldEarned"]
+        participant["champLevel"] = row["champLevel"]
+
+        participant["item_list"] = []
+        for i in range(7):
+            participant["item_list"].append(row["item"+str(i)])
+
+        match["info"].append(participant)
+
+    for row in match["info"]:
+
+        row["champion"] = champ_dict[str(row["champion"])]
+
+        row["ss1"] = ss_dict[str(row["ss1"])]
+        row["ss2"] = ss_dict[str(row["ss2"])]
+
+    return match
